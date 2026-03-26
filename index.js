9a7f21a013561f2bc25a3d916486029e1b4b0d3d
Jaseem24 <folawemi.jaseem@gmail.com>
2026-03-26 01:38:21 +0100

    update

--- index.js
+++ index.js
@@ -1,62 +1,48 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const app = express();
 
-const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 10000;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.static(__dirname))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb+srv://folawemijaseem_db_user:Jaseem%4025@cluster0.l3qzmea.mongodb.net/?retryWrites=true&w=majority')
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-const kittySchema = new mongoose.Schema({
-  name: String
-}, { timestamps: true })
+const uri = 'mongodb+srv://folawemijaseem_db_user:Jaseem%4025@cluster0.l3qzmea.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(uri)
+  .then(() => console.log("Connected"))
+  .catch(err => console.error(err));
 
-const Kitten = mongoose.model('cat', kittySchema)
+const kittySchema = new mongoose.Schema({ name: String });
+const Kitten = mongoose.model('Kitten', kittySchema);
 
-// CREATE
-app.post('/api/submit-cat', async (req, res) => {
-  try {
-    const newCat = new Kitten({ name: req.body.catName })
-    await newCat.save()
-    res.json(newCat)
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
-})
-
-// READ ALL
 app.get('/api/cats', async (req, res) => {
-  const cats = await Kitten.find().sort({ createdAt: -1 })
-  res.json(cats)
-})
+    const cats = await Kitten.find();
+    res.json(cats);
+});
 
-// READ ONE
-app.get('/api/cats/:id', async (req, res) => {
-  const cat = await Kitten.findById(req.params.id)
-  res.json(cat)
-})
+app.post('/api/submit-cat', async (req, res) => {
+    const newKitten = new Kitten({ name: req.body.catName });
+    await newKitten.save();
+    res.json({ msg: "Saved" });
+});
 
-// UPDATE
 app.put('/api/cats/:id', async (req, res) => {
-  const updated = await Kitten.findByIdAndUpdate(
-    req.params.id,
-    { name: req.body.catName },
-    { new: true }
-  )
-  res.json(updated)
-})
+    await Kitten.findByIdAndUpdate(req.params.id, { name: req.body.catName });
+    res.json({ msg: "Updated" });
+});
 
-// DELETE
 app.delete('/api/cats/:id', async (req, res) => {
-  await Kitten.findByIdAndDelete(req.params.id)
-  res.json({ msg: 'Deleted' })
-})
+    await Kitten.findByIdAndDelete(req.params.id);
+    res.json({ msg: "Deleted" });
+});
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => {
+    console.log(`Server running`);
+});
